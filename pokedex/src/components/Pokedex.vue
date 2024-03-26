1b58dfe090b78871bc51eed9830e4df62804a278
Anselm Frohebrg <anselm.frohberg@assistdigital.com>
2024-03-26 15:27:37 +0100

    130328 - Erstelung von Typs und der PokeApiService datei sowie verbesserung der fetch metode

--- pokedex/src/components/Pokedex.vue
+++ pokedex/src/components/Pokedex.vue
@@ -5,31 +5,29 @@
       <input type="text" v-model="searchQuery" placeholder="Suchen..." />
     </div>
     <div class="grid">
-        <div v-for="pokemon in filteredPokemons" :key="pokemon.name" class="card">
-          <router-link :to="`/stats/${pokemon.name}`">
+      <div v-for="pokemon in filteredPokemons" :key="pokemon.name" class="card">
+        <router-link :to="`/stats/${pokemon.name}`">
           <p>{{ pokemon.name }}</p>
         </router-link>
-        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-const pokemons = ref<Array<{ name: string }>>([]);
-const searchQuery = ref("");
+import { ref, computed, onMounted } from "vue";
+import { PokeApiService, type searchItem } from "@/services/PokeApiService";
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=1025")
-  .then(response => {
-      if (!response.ok) {
-        throw new Error('Netzwerkantwort war nicht ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      pokemons.value = data.results;
-    })
-    .catch(error => console.error('Fehler beim Fetchen der Pokémon-Daten:', error));
+const pokemons = ref<Array<searchItem>>([]);
+const searchQuery = ref("");
+const Api = new PokeApiService();
+
+const loadData = async () => {
+  pokemons.value = await Api.Name(1025);
+};
+
+onMounted(loadData);
+
 const filteredPokemons = computed(() => {
   return pokemons.value.filter((pokemon) =>
     pokemon.name.toLowerCase().includes(searchQuery.value.toLowerCase())
--- pokedex/src/components/Stats.vue
+++ pokedex/src/components/Stats.vue
@@ -1,39 +1,34 @@
 <template>
-    <div>
-      <h1>Stats von: {{ pokemonName }}</h1>
-      <router-link to="/">Zurück zum Pokedex</router-link>
-      <div v-if="stats">
+  <div>
+    <h1>Stats von: {{ pokemonName }}</h1>
+    <router-link to="/">Zurück zum Pokedex</router-link>
+    <div v-if="pokemon?.stats">
       <h2>Stats</h2>
       <ul>
-        <li v-for="stat in stats" :key="stat.stat.name">{{ stat.stat.name }}: {{ stat.base_stat }}</li>
+        <li v-for="stat in pokemon?.stats" :key="stat.stat.name">
+          {{ stat.stat.name }}: {{ stat.base_stat }}
+        </li>
       </ul>
     </div>
     <div v-else>
       <p>Lade Daten...</p>
     </div>
-    </div>
-  </template>
+  </div>
+</template>
 
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
-import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+import { ref, onMounted, Ref } from "vue";
+import { PokeApiService, type Pokemon } from "@/services/PokeApiService";
 
 const route = useRoute();
 const pokemonName = route.params.name;
-const stats = ref(null);
+const pokemon : Ref<Pokemon | null>= ref(null);
+const Api = new PokeApiService();
 
+const loadData = async () => {
+  pokemon.value = await Api.get(pokemonName as string);
+};
 
-onMounted(() => {
-  fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Netzwerkantwort war nicht ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      stats.value = data.stats;
-    })
-    .catch(error => console.error('Fehler beim Fetchen der Pokémon-Daten:', error));
-});
+onMounted(loadData);
 </script>
